69234a4708822135b27d8df215c7c576e7117be2
Egorkud <egorkud2005@gmail.com>
2025-12-09 04:17:45 +0200

    feat(lab2): update client_udp.py

--- lab2/client_udp.py
+++ lab2/client_udp.py
@@ -7,11 +7,13 @@ PORT = 5001
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 message = input("Enter message: ")
-start = time.perf_counter()
-client.sendto(message.encode(), (HOST, PORT))
 
-data, addr = client.recvfrom(1024)
-end = time.perf_counter()
+for i in range(100):
+    start = time.perf_counter()
+    client.sendto(message.encode(), (HOST, PORT))
 
-print(end - start)
-print("Server:", data.decode())
+    data, addr = client.recvfrom(1024)
+    end = time.perf_counter()
+
+    print(end - start)
+    print("Server:", data.decode())
