e790eecf6c8a2f61d178a041afbc360d9d431b51
Egorkud <egorkud2005@gmail.com>
2025-12-09 04:21:00 +0200

    feat(lab2): update client_udp.py

--- lab2/client_udp.py
+++ lab2/client_udp.py
@@ -5,15 +5,21 @@ HOST = '127.0.0.1'
 PORT = 5001
 
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client.settimeout(1)
 
 message = input("Enter message: ")
 
 for i in range(100):
-    start = time.perf_counter()
-    client.sendto(message.encode(), (HOST, PORT))
+    try:
+        start = time.perf_counter()
+        client.sendto(message.encode(), (HOST, PORT))
 
-    data, addr = client.recvfrom(1024)
-    end = time.perf_counter()
+        data, addr = client.recvfrom(1024)
+        end = time.perf_counter()
 
-    print(end - start)
-    print("Server:", data.decode())
+        print(f"Iteration {i}: {end - start:.6f} sec")
+        print("Server:", data.decode())
+
+    except socket.timeout:
+        print(f"Iteration {i}: Timeout!")
+        continue
