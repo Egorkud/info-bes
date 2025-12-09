e11dc9d04ab856d0d8a9e6f1c24ab6eda70030a3
Egorkud <egorkud2005@gmail.com>
2025-12-09 04:13:46 +0200

    feat(lab2): update server_udp.py

--- lab2/server_udp.py
+++ lab2/server_udp.py
@@ -1,5 +1,7 @@
 import socket
-import time
+import random
+
+LOSS_PROBABILITY = 0.3
 
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server.bind(('127.0.0.1', 5001))
@@ -9,5 +11,10 @@ while True:
     data, addr = server.recvfrom(1024)
     print(f"Client {addr} sent {data}")
 
+    if random.random() < LOSS_PROBABILITY:
+        print("Package lost! No response!")
+        continue
+
     response = data.decode().upper().encode()
     server.sendto(response, addr)
+    print(f"Data sent {data.decode()} adress: {addr}")
