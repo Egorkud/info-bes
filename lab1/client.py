9f2b3e131f08049289a49313d93264d5eb5d14d5
Egorkud <egorkud2005@gmail.com>
2025-12-09 04:08:32 +0200

    feat(lab2): update client tcp

--- lab1/client.py
+++ lab1/client.py
@@ -1,4 +1,5 @@
 import socket
+import time
 
 HOST = '127.0.0.1'
 PORT = 65432
@@ -6,6 +7,12 @@ PORT = 65432
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     message = input("Введіть повідомлення: ")
+
+    start = time.perf_counter()
     s.sendall(message.encode())
+
     data = s.recv(1024)
+    end = time.perf_counter()
+
+    print(end - start)
     print("Від сервера:", data.decode())
